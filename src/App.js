82c73ff760f05c2a72775d8a7b0f0862e9f92421
chennareddy44 <20btrcs023@jainuniversity.ac.in>
2024-02-12 02:06:14 +0530

    refactor: Restructure app routing and layout components - Moved the routing logic to the root App component for better organization - Refactored Body component to use react-router's Outlet for nested routing - Updated imports and removed unnecessary components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import Head from "./components/Head";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./utils/store";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import WatchPage from "./components/WatchPage";
+import MainContainer from "./components/MainContainer";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watchpage",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <>
-      <Head />
-      <Body />
-    </>
+    <Provider store={store}>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
   );
 };
 
