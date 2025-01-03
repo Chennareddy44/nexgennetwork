4a5ce434622714da864ead61afb90e8097f27f90
chennareddy44 <20btrcs023@jainuniversity.ac.in>
2025-01-04 00:56:23 +0530

    Change the search api

---
+++ src/utils/constants.js
@@ -0,0 +1,99 @@
+const API_KEY = "AIzaSyAE2wg2_nvEnNDP2SVrLDz8G5FgW9afHsU";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=US&key=" +
+  API_KEY;
+
+export const YOUTUBE_SEARCH_API =
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=10&key= " +
+  API_KEY +
+  " &q=";
+
+// "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const commentsData = [
+  {
+    name: "Chael Sonnen",
+    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+    replies: [
+      {
+        name: "Chael Sonnen",
+        text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+        replies: [
+          {
+            name: "Chael Sonnen",
+            text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+            replies: [
+              {
+                name: "Chael Sonnen",
+                text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+                replies: [
+                  {
+                    name: "Chael Sonnen",
+                    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            name: "Chael Sonnen",
+            text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+            replies: [],
+          },
+          {
+            name: "Chael Sonnen",
+            text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+            replies: [],
+          },
+          {
+            name: "Chael Sonnen",
+            text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: "Chael Sonnen",
+        text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+        replies: [],
+      },
+      {
+        name: "Chael Sonnen",
+        text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+        replies: [],
+      },
+      {
+        name: "Chael Sonnen",
+        text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Chael Sonnen",
+    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+    replies: [],
+  },
+  {
+    name: "Chael Sonnen",
+    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+    replies: [],
+  },
+  {
+    name: "Chael Sonnen",
+    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+    replies: [],
+  },
+  {
+    name: "Chael Sonnen",
+    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+    replies: [],
+  },
+  {
+    name: "Chael Sonnen",
+    text: "Listen guys, if you’re not willing to go too far, you will never go far enough in life!",
+    replies: [],
+  },
+];
